2e7362e00fb51fea341298d88599aaf20e71d495
Markus Knittig <markus@myd0d.de>
2008-09-21 00:20:59 +0200

    Fixed permissions

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -19,10 +19,10 @@ REDMINE_SUPPORTED_SCM = %w( Subversion Darcs Mercurial Cvs Bazaar Git Filesystem
 Redmine::AccessControl.map do |map|
   map.permission :view_project, {:projects => [:show, :activity]}, :public => true
   map.permission :search_project, {:search => :index}, :public => true
-  map.permission :edit_project, {:projects => [:settings, :edit]}, :require => :member
+  map.permission :edit_project, {:projects => [:settings, :edit, :update]}, :require => :member
   map.permission :select_project_modules, {:projects => :modules}, :require => :member
-  map.permission :manage_members, {:projects => :settings, :members => [:new, :edit, :destroy]}, :require => :member
-  map.permission :manage_versions, {:projects => [:settings, :add_version], :versions => [:edit, :destroy]}, :require => :member
+  map.permission :manage_members, {:projects => :settings, :members => [:new, :create, :edit, :update, :destroy]}, :require => :member
+  map.permission :manage_versions, {:projects => [:settings, :add_version], :versions => [:edit, :update, :destroy]}, :require => :member
   
   map.project_module :issue_tracking do |map|
     # Issue categories
@@ -33,40 +33,40 @@ Redmine::AccessControl.map do |map|
                                   :versions => [:show, :status_by],
                                   :queries => :index,
                                   :reports => :issue_report}, :public => true                    
-    map.permission :add_issues, {:issues => :new}
-    map.permission :edit_issues, {:issues => [:edit, :reply, :bulk_edit, :destroy_attachment]}
+    map.permission :add_issues, {:issues => [:new, :create]}
+    map.permission :edit_issues, {:issues => [:edit, :update, :reply, :bulk_edit, :destroy_attachment]}
     map.permission :manage_issue_relations, {:issue_relations => [:new, :destroy]}
-    map.permission :add_issue_notes, {:issues => [:edit, :reply]}
-    map.permission :edit_issue_notes, {:journals => :edit}, :require => :loggedin
-    map.permission :edit_own_issue_notes, {:journals => :edit}, :require => :loggedin
+    map.permission :add_issue_notes, {:issues => [:edit, :update, :reply]}
+    map.permission :edit_issue_notes, {:journals => [:edit, :update]}, :require => :loggedin
+    map.permission :edit_own_issue_notes, {:journals => [:edit, :update]}, :require => :loggedin
     map.permission :move_issues, {:issues => :move}, :require => :loggedin
     map.permission :delete_issues, {:issues => :destroy}, :require => :member
     # Queries
-    map.permission :manage_public_queries, {:queries => [:new, :edit, :destroy]}, :require => :member
-    map.permission :save_queries, {:queries => [:new, :edit, :destroy]}, :require => :loggedin
+    map.permission :manage_public_queries, {:queries => [:new, :create, :edit, :create, :destroy]}, :require => :member
+    map.permission :save_queries, {:queries => [:new, :create, :edit, :upate, :destroy]}, :require => :loggedin
     # Gantt & calendar
     map.permission :view_gantt, :issues => :gantt
     map.permission :view_calendar, :issues => :calendar
     # Watchers
     map.permission :view_issue_watchers, {}
-    map.permission :add_issue_watchers, {:watchers => :new}
+    map.permission :add_issue_watchers, {:watchers => [:new, :create]}
   end
   
   map.project_module :time_tracking do |map|
-    map.permission :log_time, {:timelog => :edit}, :require => :loggedin
+    map.permission :log_time, {:timelog => [:edit, :update]}, :require => :loggedin
     map.permission :view_time_entries, :timelog => [:details, :report]
-    map.permission :edit_time_entries, {:timelog => [:edit, :destroy]}, :require => :member
-    map.permission :edit_own_time_entries, {:timelog => [:edit, :destroy]}, :require => :loggedin
+    map.permission :edit_time_entries, {:timelog => [:edit, :update, :destroy]}, :require => :member
+    map.permission :edit_own_time_entries, {:timelog => [:edit, :update, :destroy]}, :require => :loggedin
   end
   
   map.project_module :news do |map|
-    map.permission :manage_news, {:news => [:new, :edit, :destroy, :destroy_comment]}, :require => :member
+    map.permission :manage_news, {:news => [:new, :create, :edit, :update, :destroy, :destroy_comment]}, :require => :member
     map.permission :view_news, {:news => [:index, :show]}, :public => true
     map.permission :comment_news, {:news => :add_comment}
   end
 
   map.project_module :documents do |map|
-    map.permission :manage_documents, {:documents => [:new, :edit, :destroy, :add_attachment, :destroy_attachment]}, :require => :loggedin
+    map.permission :manage_documents, {:documents => [:new, :create, :edit, :update, :destroy, :add_attachment, :destroy_attachment]}, :require => :loggedin
     map.permission :view_documents, :documents => [:index, :show, :download]
   end
   
@@ -76,27 +76,27 @@ Redmine::AccessControl.map do |map|
   end
     
   map.project_module :wiki do |map|
-    map.permission :manage_wiki, {:wikis => [:edit, :destroy]}, :require => :member
+    map.permission :manage_wiki, {:wikis => [:edit, :update, :destroy]}, :require => :member
     map.permission :rename_wiki_pages, {:wiki => :rename}, :require => :member
     map.permission :delete_wiki_pages, {:wiki => :destroy}, :require => :member
     map.permission :view_wiki_pages, :wiki => [:index, :special]
     map.permission :view_wiki_edits, :wiki => [:history, :diff, :annotate]
-    map.permission :edit_wiki_pages, :wiki => [:edit, :preview, :add_attachment, :destroy_attachment]
+    map.permission :edit_wiki_pages, :wiki => [:edit, :update, :preview, :add_attachment, :destroy_attachment]
     map.permission :protect_wiki_pages, {:wiki => :protect}, :require => :member
   end
     
   map.project_module :repository do |map|
-    map.permission :manage_repository, {:repositories => [:edit, :destroy]}, :require => :member
+    map.permission :manage_repository, {:repositories => [:edit, :update, :destroy]}, :require => :member
     map.permission :browse_repository, :repositories => [:show, :browse, :entry, :annotate, :changes, :diff, :stats, :graph]
     map.permission :view_changesets, :repositories => [:show, :revisions, :revision]
     map.permission :commit_access, {}
   end
 
   map.project_module :boards do |map|
-    map.permission :manage_boards, {:boards => [:new, :edit, :destroy]}, :require => :member
+    map.permission :manage_boards, {:boards => [:new, :create, :edit, :update, :destroy]}, :require => :member
     map.permission :view_messages, {:boards => [:index, :show], :messages => [:show]}, :public => true
-    map.permission :add_messages, {:messages => [:new, :reply, :quote]}
-    map.permission :edit_messages, {:messages => :edit}, :require => :member
+    map.permission :add_messages, {:messages => [:new, :create, :reply, :quote]}
+    map.permission :edit_messages, {:messages => [:edit, :update]}, :require => :member
     map.permission :delete_messages, {:messages => :destroy}, :require => :member
   end
 end
