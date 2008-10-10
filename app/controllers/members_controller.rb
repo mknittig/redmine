d88ee1549923dc27b54a92e2fddf97e597445bbf
Markus Knittig <markus@myd0d.de>
2008-10-10 23:12:02 +0200

    Revert

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -54,7 +54,7 @@ class MembersController < ApplicationController
 
 private
   def find_project
-    @project = Project.find(params[:id])
+    @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
   end
